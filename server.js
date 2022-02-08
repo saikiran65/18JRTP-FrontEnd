7e9d099b6fc8e734be8c7821735108492e0d7992
nkirangoud <nkirangoud1@gmail.com>
2022-02-09 01:02:37 +0530

    name should be as it is in package.json

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/MinPro2'));
+app.use(express.static('./dist/min-pro2'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/MinPro2/'}),
+    res.sendFile('index.html', {root: 'dist/min-pro2/'}),
 );
 
 // Start the app by listening on the default Heroku port
